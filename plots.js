6f696a44f306a9af43f1c9fda85f76b85208666d
icycake7 <110522677+icycake7@users.noreply.github.com>
2022-11-02 18:18:05 -0500

    adding Challenge files

---
+++ plots.js
@@ -0,0 +1,41 @@
+function init() {
+  var selector = d3.select("#selDataset");
+
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+})}
+
+init();
+
+// function optionChanged(newSample) {
+//   console.log(newSample);
+// }
+
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    PANEL.append("h6").text(`location: ${result.location}`);
+    PANEL.append("h6").text(`bbtype: ${result.bbtype}`);
+    PANEL.append("h6").text(`ethnicity: ${result.ethnicity}`);
+    PANEL.append("h6").text(`gender: ${result.gender}`);
+    PANEL.append("h6").text(`ID: ${result.id}`);
+    PANEL.append("h6").text(`wfrq: ${result.wfreq}`);
+  });
+}
